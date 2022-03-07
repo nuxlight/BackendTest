ac7a89cb28ed0769a6361472cc749559057e4e25
Thibaud Pellissier <thibaudpellissier@pltmail.fr>
2022-03-07 15:16:29 -0400

    🐛 [Dev] Fix wording in errors messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,17 @@ class UserValidator < ActiveModel::Validator
     def validate(record)
         alpha = "ABCDEFGHIJKLMNOPKRSTUVWXYZ"
         if record.name.length != 3
-            record.errors[:errors] << "User should have 3 characters"
+            record.errors[:errors] << "Username should have 3 characters"
             return
         end
         # Test if entry user is not in lowercase or a number 
         record.name.each_char do |l|
             if l.match(/\d+/) != nil
-                record.errors[:errors] << "User name should not have a number"
+                record.errors[:errors] << "Username should not contain a number"
                 return
             end
             if l.match(/\p{Upper}/) == nil
-                record.errors[:errors] << "User name should be in uppercase"
+                record.errors[:errors] << "Username should be in uppercase"
                 return
             end
         end
