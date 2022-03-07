680c919e5b7868ee3c5b9408c62792b86006238d
Thibaud Pellissier <thibaudpellissier@pltmail.fr>
2022-03-07 12:54:04 -0400

    🐛 [Dev] Add number test and uppercase test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,18 @@ class UserValidator < ActiveModel::Validator
         alpha = "ABCDEFGHIJKLMNOPKRSTUVWXYZ"
         if record.name.length != 3
             record.errors[:errors] << "User should have 3 characters"
+            return
+        end
+        # Test if entry user is not in lowercase or a number 
+        record.name.each_char do |l|
+            if l.match(/\d+/) != nil
+                record.errors[:errors] << "User name should not have a number"
+                return
+            end
+            if l.match(/\p{Upper}/) == nil
+                record.errors[:errors] << "User name should be in uppercase"
+                return
+            end
         end
         if User.find_by(name: record.name) != nil
             # User exist in database, try to generate a new user
