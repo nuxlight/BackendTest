9a46c8e43a802a406b3279f4acf4ba3556ab512f
Thibaud Pellissier <thibaudpellissier@pltmail.fr>
2022-03-16 19:40:27 +0100

    ♻️ Simplifying user model, and use rails validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,6 @@
-class UserValidator < ActiveModel::Validator
-    def validate(record)
-        alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        if record.name.length != 3
-            record.errors[:errors] << "Username should have 3 characters"
-            return
-        end
-        # Test if entry user is not in lowercase or a number 
-        record.name.each_char do |l|
-            if l.match(/\d+/) != nil
-                record.errors[:errors] << "Username should not contain a number"
-                return
-            end
-            if l.match(/\p{Upper}/) == nil
-                record.errors[:errors] << "Username should be in uppercase"
-                return
-            end
-        end
-        if User.find_by(name: record.name) != nil
-            # User exist in database, try to generate a new user
-            while User.find_by(name: record.name) != nil
-                record.name = alpha[rand(26)]+alpha[rand(26)]+alpha[rand(26)]
-            end
-        end
-    end
-end
 class User < ApplicationRecord
-    validates_with UserValidator
+    validates_presence_of :name, :message => "Username should be present" 
+    validates :name, length: {minimum: 3, maximum: 3}
+    validates :name, format: { with: /\A[A-Z]+\z/,
+        message: "Username should not contain a number and it should be in uppercase" }
 end
